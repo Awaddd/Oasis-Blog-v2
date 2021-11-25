d7b87fa1943e57edca522d12aa6a6ac2865c4b35
Awad <awad@moblox.com>
2021-11-25 22:32:54 +0000

    Added burger and improved nav design

--- frontend/src/layout/Nav.tsx
+++ frontend/src/layout/Nav.tsx
@@ -1,17 +1,32 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import Link from 'next/link';
+import { Burger } from '@mantine/core';
 
-const Nav = ({ meta }: { meta: ReactNode }) => (
-  <nav className="grid w-full grid-cols-2 p-4 antialiased md:py-7">
-    {meta}
-    <div className=" brand">
-      Omar Dini<span className="text-primary">.</span>
-    </div>
-    <div className="flex items-end justify-end gap-4">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-    </div>
-  </nav>
-);
+const Nav = ({ meta }: { meta: ReactNode }) => {
+
+  const [open, setOpen] = useState(false)
+
+  return (
+    <nav className={`grid items-end w-full grid-flow-col-dense py-4 antialiased global-padding ${open && "bg-gray-900 text-gray-200 md:bg-white md:text-black"}`}>
+      {meta}
+      <div className="brand justify-self-start">
+        Omar Dini<span className={`${open ? "text-pink-500" : "text-primary"}`}>.</span>
+      </div>
+      <div className="relative justify-self-end md:hidden burger">
+        <Burger opened={open} onClick={() => setOpen(open => !open)} size="md" color={`${open ? "white" : "black"}`} />
+      </div>
+      {open && (
+        <div className="flex flex-col col-start-1 col-end-3 row-start-2 gap-2 py-4 md:hidden">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </div>
+      )}
+      <div className="justify-end hidden gap-6 justify-self-end md:flex">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </nav>
+  );
+}
 
 export default Nav;
