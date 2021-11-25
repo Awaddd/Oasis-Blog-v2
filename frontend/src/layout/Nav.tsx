437535a524698bec4f4f397740ed69a93924fa23
Awad <awad@moblox.com>
2021-11-25 23:28:19 +0000

    Added categories to nav

--- frontend/src/layout/Nav.tsx
+++ frontend/src/layout/Nav.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { Key, ReactNode, useState } from 'react';
 import Link from 'next/link';
 import { Burger } from '@mantine/core';
 import { gql } from 'graphql-request';
@@ -18,28 +18,31 @@ const Nav = ({ meta }: { meta: ReactNode }) => {
 
   return (
     <nav className={`grid items-end w-full grid-flow-col-dense py-4 antialiased global-padding ${open && "bg-gray-900 text-gray-200 md:bg-white md:text-black"}`}>
+
       {meta}
+
       <div className="brand justify-self-start">
         Omar Dini<span className={`${open ? "text-pink-500" : "text-primary"}`}>.</span>
       </div>
+
       <div className="relative justify-self-end md:hidden burger">
         <Burger opened={open} onClick={() => setOpen(open => !open)} size="md" color={`${open ? "white" : "black"}`} />
       </div>
+
       {open && (
         <div className="flex flex-col col-start-1 col-end-3 row-start-2 gap-2 py-4 md:hidden">
           <Link href="/">Home</Link>
-          {categories.map(({ id, name, pluralName }: Category, key) => (
+          {categories.map(({ pluralName }: Category, key: Key | null | undefined) => (
             <Link href={`/${pluralName}`} key={key}>{pluralName}</Link>
           ))}
         </div>
       )}
+
       <div className="justify-end hidden gap-6 justify-self-end md:flex">
         <Link href="/">Home</Link>
-
-        {categories.map(({ id, name, pluralName }: Category, key) => (
+        {categories.map(({ pluralName }: Category, key: Key | null | undefined) => (
           <Link href={`/${pluralName}`} key={key}>{pluralName}</Link>
         ))}
-
       </div>
     </nav>
   );
