9fd12b6b809fb95005afd518986621f76df0c69e
Awad <awad@moblox.com>
2021-11-25 22:48:25 +0000

    Updated footer styles

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import { AppConfig } from '../utils/AppConfig';
 
 const Footer = () => (
-  <div className="self-end w-full p-4 antialiased border-t border-gray-300 md:py-7">
+  <div className="self-end w-full py-4 antialiased border-t border-gray-300 global-padding md:py-7">
     <div className="text-sm text-center">
       © Copyright {new Date().getFullYear()} {AppConfig.title}
     </div>
