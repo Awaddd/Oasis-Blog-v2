2e2174ccf160814711d7512dcd2f1571deb5feb1
Awad <awad@moblox.com>
2021-11-27 14:36:41 +0000

    Added active link to nav

--- frontend/src/layout/Nav.tsx
+++ frontend/src/layout/Nav.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { Key, ReactNode, useState } from 'react';
 import Link from 'next/link';
 import { Burger } from '@mantine/core';
@@ -6,6 +7,9 @@ import { useQuery } from 'react-query';
 import { client } from '../services/api';
 
 const Nav = ({ meta }: { meta: ReactNode }) => {
+
+  const router = useRouter()
+  const category = router.query.category
 
   const [open, setOpen] = useState(false)
 
@@ -30,17 +34,27 @@ const Nav = ({ meta }: { meta: ReactNode }) => {
         <>
           {open && (
             <div className="flex flex-col col-start-1 col-end-3 row-start-2 gap-2 py-4 md:hidden">
-              <Link href="/">Home</Link>
+              <Link href="/">
+                <a className={`${router.pathname === '/' && 'text-pink-500'}`}>Home</a>
+              </Link>
+
               {data.categories.map(({ pluralName }: Category, key: Key | null | undefined) => (
-                <Link href={`/${pluralName}`} key={key}>{pluralName}</Link>
+                <Link href={`/${pluralName}`} key={key}>
+                  <a className={`${category === pluralName && 'text-pink-500'}`}>{pluralName}</a>
+                </Link>
               ))}
             </div>
           )}
 
           <div className="justify-end hidden gap-6 justify-self-end md:flex">
-            <Link href="/">Home</Link>
+            <Link href="/">
+              <a className={`${router.pathname === '/' && 'text-primary'}`}>Home</a>
+            </Link>
+
             {data.categories.map(({ pluralName }: Category, key: Key | null | undefined) => (
-              <Link href={`/${pluralName}`} key={key}>{pluralName}</Link>
+              <Link href={`/${pluralName}`} key={key}>
+                <a className={`${category === pluralName && 'text-primary'}`}>{pluralName}</a>
+              </Link>
             ))}
           </div>
         </>
