6155394ae0ba1be032e9077b1a6b7050287e4d41
Awad <awad@moblox.com>
2021-12-11 20:21:56 +0000

    Featured article additional info row now displayed above the hero image on mobile devices

--- frontend/src/components/FeaturedArticle.tsx
+++ frontend/src/components/FeaturedArticle.tsx
@@ -15,14 +15,20 @@ const FeaturedArticle = () => {
     <div className="flex flex-col-reverse md:grid md:grid-cols-2 md:gap-4">
       <div className="md:grid">
         <div className="text-center md:justify-self-start md:text-left mt-sm md:mt-md">
-          <div className="grid grid-cols-2">
-            <p className="justify-self-start text-primary">Featured</p>
-            <p className="justify-self-end lg:hidden">Nov. 11</p>
-          </div>
+          <header className="hidden md:block">
+            <div className="grid grid-cols-2">
+              <p className="justify-self-start text-primary">Featured</p>
+              <p className="justify-self-end lg:hidden">Nov. 11</p>
+            </div>
+          </header>
           <h1 className="text-2xl font-bold leading-7 mt-sm md:leading-10 md:underline md:text-4xl">{title}</h1>
         </div>
       </div>
       <div className="mt-md md:mt-0">
+        <div className="grid grid-cols-2 mb-xs md:hidden">
+          <p className="justify-self-start text-primary">Featured</p>
+          <p className="justify-self-end lg:hidden">Nov. 11</p>
+        </div>
         <img src={`http://localhost:1337${image.url}`} alt="cover image" className="object-cover w-full h-48 rounded-lg sm:rounded-md sm:h-52 md:h-64 lg:h-80" />
       </div>
     </div>
