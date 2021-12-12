ac1e7cf417bc8441f3a7a34c237f767cfb51174e
Awad <awad@moblox.com>
2021-12-12 19:10:55 +0000

    Touchup fonts and margins

--- frontend/src/components/ArticleCard.tsx
+++ frontend/src/components/ArticleCard.tsx
@@ -11,8 +11,8 @@ const ArticleCard = ({ data }: { data: Article }) => {
             <img src={`http://localhost:1337${image.url}`} alt="cover image" className="object-cover w-full h-48 rounded-lg" />
           </div>
         )}
-        <div className="mt-2.5">
-          <h2 className="text-2xl font-bold leading-7">{title}</h2>
+        <div className="mt-2.5 mx-2.5">
+          <h2 className="text-2xl font-semibold leading-7">{title}</h2>
           {subtitle && <p className="font-medium mt-xs">{subtitle}</p>}
         </div>
       </article>
@@ -21,7 +21,7 @@ const ArticleCard = ({ data }: { data: Article }) => {
 
   return (
     <article className="h-48 border-l-4 shadow cursor-pointer border-primary py-lg my-sm md:my-0" style={{ paddingLeft: "26px", paddingRight: "26px" }}>
-      <h2 className="text-2xl font-bold leading-7">{title}</h2>
+      <h2 className="text-2xl font-semibold leading-7">{title}</h2>
       {subtitle && <p className="font-medium mt-xs">{subtitle}</p>}
     </article>
   )
