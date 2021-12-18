84ecbcadd02f4b0584c32519d9377ec0fe4506ef
Awad <awad@moblox.com>
2021-12-18 01:59:02 +0000

    Api config refactor

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 
-const env = process.env.NODE_ENV;
+const dev = process.env.NODE_ENV !== "production";
 
-let host: string = "";
-let domain: string = "";
-
-if (env === "development") {
-  host = "http://localhost:1337/graphql";
-  domain = "http://localhost:1337";
-}
-
-export const client = new GraphQLClient(host);
-export const api = domain;
+export const api = dev ? "http://localhost:1337" : "";
+export const client = new GraphQLClient(
+  dev ? "http://localhost:1337/graphql" : ""
+);
