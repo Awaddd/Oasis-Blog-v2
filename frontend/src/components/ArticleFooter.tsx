f8b88ebf684114685c5399747ab0a7d41e9e05cf
Awad <awad@moblox.com>
2021-12-18 18:36:41 +0000

    Made article footer social icons dark

--- frontend/src/components/ArticleFooter.tsx
+++ frontend/src/components/ArticleFooter.tsx
@@ -2,10 +2,10 @@ import { FC } from 'react';
 import SocialIconBar from './SocialIconBar';
 
 const ArticleFooter: FC = () => (
-  <footer className="flex flex-col text-center mt-md md:mt-lg footer py-md md:py-lg">
+  <footer className="flex flex-col text-center mt-md md:mt-lg footer py-[24px] md:py-lg px-lg">
     <p className="text-sm text-gray-900">Thanks for reading, make sure to check back for weekly updates!</p>
-    <div className="flex self-center md:mt-[11px]">
-      <SocialIconBar />
+    <div className="flex self-center mt-md">
+      <SocialIconBar dark={true} />
     </div>
   </footer>
 )
