47cbc1532049b488c23ee112adb1880509b959b5
Awad <awad@moblox.com>
2021-12-18 18:39:27 +0000

    Updated article footer content

--- frontend/src/components/ArticleFooter.tsx
+++ frontend/src/components/ArticleFooter.tsx
@@ -3,8 +3,9 @@ import SocialIconBar from './SocialIconBar';
 
 const ArticleFooter: FC = () => (
   <footer className="flex flex-col text-center mt-md md:mt-lg footer py-[24px] md:py-lg px-lg">
-    <p className="text-sm text-gray-900">Thanks for reading, make sure to check back for weekly updates!</p>
-    <div className="flex self-center mt-md">
+    <h4 className="font-bold text-gray-900">Thanks for reading</h4>
+    <p className="text-sm">Make sure to check back for weekly updates!</p>
+    <div className="flex self-center mt-[16px]">
       <SocialIconBar dark={true} />
     </div>
   </footer>
