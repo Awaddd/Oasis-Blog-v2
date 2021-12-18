7835f7255eb5ae1dec8234a31b48363aa9a4353b
Awad <awad@moblox.com>
2021-12-18 19:06:46 +0000

    Added colour and classes to main

--- frontend/src/templates/Main.tsx
+++ frontend/src/templates/Main.tsx
@@ -5,15 +5,17 @@ import Footer from '../layout/Footer'
 
 type IMainProps = {
   meta: ReactNode;
+  color?: string;
+  classes?: string;
   footerProps?: { dark?: boolean; }
   footer?: FC | JSX.Element;
   children: ReactNode;
 };
 
-const Main = ({ meta, footerProps, footer, children }: IMainProps) => (
+const Main = ({ meta, color, classes, footerProps, footer, children }: IMainProps) => (
   <div className="flex flex-col w-full h-full antialiased font-inter">
     <Nav meta={meta} />
-    <main className="h-full global-padding">{children}</main>
+    <main className={`h-full global-padding ${color} ${classes}`}>{children}</main>
     <Footer {...footerProps}>
       {footer && footer}
     </Footer>
