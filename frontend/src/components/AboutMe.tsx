b2cbfae8307e5d56b2add993ff87ede5b7bd8549
Awad <awad@moblox.com>
2021-12-21 22:24:05 +0000

    Adjusted about me padding

--- frontend/src/components/AboutMe.tsx
+++ frontend/src/components/AboutMe.tsx
@@ -4,7 +4,7 @@ const AboutMe = () => (
   <main className="bg-gray-900 global-padding py-lg md:py-xl">
     <div className="container mx-auto 2xl:w-9/12">
       <div className="items-center lg:flex">
-        <article className="lg:w-1/2">
+        <article className="lg:w-1/2 lg:px-xl md:px-[45px]">
           <h2 className="text-3xl font-bold text-gray-100">Omar Dini</h2>
 
           <p className="text-gray-400 lg:max-w-md">
