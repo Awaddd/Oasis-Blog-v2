e4e9371621ad042f52969217008854fbb5b2f03d
Awad <awad@moblox.com>
2021-12-21 22:08:33 +0000

    Improved the design of the article page

--- frontend/src/pages/article/[slug].tsx
+++ frontend/src/pages/article/[slug].tsx
@@ -43,28 +43,29 @@ const Article = ({ article, imageProps }: { article: Article, imageProps: ImageT
   return (
     <Main meta={META} color="bg-slate-50" classes="pb-lg" footer={<ArticleFooter />} >
 
-      {image?.url && (
-        <div className="relative h-52 sm:h-60 lg:h-80 2xl:h-96 reverse-global-padding reverse-top-global-page-padding">
-          <Image layout="fill" {...imageProps} placeholder="blur" priority alt={title} className="absolute top-0 z-10 text-center text-gray-200 bg-gray-900 heroImage" />
-          <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-            <div className="absolute z-10 w-full h-full hero-image-overlay"></div>
-            <header className="z-10 text-center text-white md:w-4/5 md:text-gray-200 px-md py-sm md:py-0 md:px-0">
-              <h1 className="text-3xl md:font-semibold md:text-4xl">{title}</h1>
-            </header>
-          </div>
-        </div>
-      )}
+      <div className="lg:w-3/5 md:mx-auto my-lg mb-[45px] 2xl:w-2/4">
 
-      <div className="md:w-3/5 md:mx-auto my-lg mb-[45px] 2xl:w-2/4">
-        <div className="flex 2xl:pb-2">
-          <div className="w-12 h-12 lg:w-16 lg:h-16">
-            <img src="https://www.byrdie.com/thmb/j922lTh-Vn8HnmQsxSKGA0ztxw0=/756x756/filters:no_upscale():max_bytes(150000):strip_icc()/Rob-cc2d68e18be04acf90a74623c1087fd6.jpg" className="object-cover w-full h-full bg-red-300 rounded-full" />
+        {image?.url && (
+          <div className="relative h-52 sm:h-60 lg:h-80 2xl:h-96 mt-md">
+            <Image layout="fill" {...imageProps} placeholder="blur" priority alt={title} className="absolute top-0 z-10 text-center text-gray-200 bg-gray-900 rounded-lg heroImage" />
+            <div className="absolute top-0 left-0 grid w-full h-full">
+              <div className="absolute z-10 w-full h-full rounded-lg hero-image-overlay"></div>
+              <div className="z-10 flex flex-col w-full h-full text-center text-white px-md py-sm">
+                <header className="flex items-center justify-center flex-1 w-11/12 mx-auto">
+                  <h1 className="text-2xl tracking-tight md:text-3xl md:font-semibold lg:text-4xl 2xl:text-5xl">{title}</h1>
+                </header>
+                <footer className="flex items-end w-full">
+                  <div className="flex justify-between w-full">
+                    <div className="relative flex items-center">
+                      <span className="text-sm font-normal mt-md 2xl:text-normal">Omar Dini</span>
+                    </div>
+                    <span className="text-sm mt-md 2xl:text-normal">November 11th, 2020</span>
+                  </div>
+                </footer>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col justify-center ml-[11px]">
-            <h4 className="font-semibold text-black 2xl:text-xl">Omar Dini</h4>
-            <p className="text-sm -mt-xs 2xl:text-lg">November 11th, 2020</p>
-          </div>
-        </div>
+        )}
 
         <article className="!max-w-full prose-sm md:prose sm:prose 2xl:prose-xl mt-8">
           {parse(content)}
