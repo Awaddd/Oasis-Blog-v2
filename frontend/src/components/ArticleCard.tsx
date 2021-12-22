29bd4cbbe75609d2bf9f52f7e2113a7ca422195a
Awad <awad@moblox.com>
2021-12-22 18:37:27 +0000

    Fix build

--- frontend/src/components/ArticleCard.tsx
+++ frontend/src/components/ArticleCard.tsx
@@ -3,21 +3,21 @@ import { Article } from "../utils/types/global"
 const ArticleCard = ({ data }: { data: Article }) => {
   const { title, subtitle, image } = data
 
-  if (image) {
-    return (
-      <article className="grid cursor-pointer">
-        {image?.url && (
-          <div className="">
-            <img src={`http://localhost:1337${image.url}`} alt="cover image" className="object-cover w-full h-48 rounded-lg" />
-          </div>
-        )}
-        <div className="mt-2.5 mx-2.5">
-          <h2 className="text-xl font-semibold leading-7 text-zinc-900">{title}</h2>
-          {subtitle && <p className="font-medium md:mt-[2px]">{subtitle}</p>}
+  if (!image) return null;
+
+  return (
+    <article className="grid cursor-pointer">
+      {image?.url && (
+        <div className="">
+          <img src={`http://localhost:1337${image.url}`} alt="cover image" className="object-cover w-full h-48 rounded-lg" />
         </div>
-      </article>
-    )
-  }
+      )}
+      <div className="mt-2.5 mx-2.5">
+        <h2 className="text-xl font-semibold leading-7 text-zinc-900">{title}</h2>
+        {subtitle && <p className="font-medium md:mt-[2px]">{subtitle}</p>}
+      </div>
+    </article>
+  )
 }
 
 export default ArticleCard
