233c0465f576582bd4874671953811ec42125648
Awad <awad@moblox.com>
2021-12-22 00:43:23 +0000

    Touchups

--- frontend/src/components/ArticleFooter.tsx
+++ frontend/src/components/ArticleFooter.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import SocialIconBar from './SocialIconBar';
 
 const ArticleFooter: FC = () => (
-  <footer className="flex flex-col text-center footer py-[24px] md:py-lg px-lg bg-slate-50">
+  <footer className="flex flex-col text-center footer py-[24px] md:py-lg md:pb-[45px] px-lg bg-slate-50">
     <h4 className="font-bold text-gray-900">Thanks for reading</h4>
     <p className="text-sm">Make sure to check back for weekly updates!</p>
     <div className="flex self-center mt-[16px]">
